3bb289f502f9240ed289a1fed2a6e01f0aa12f42
Artyom <Batonovmisa@gmail.com>
2025-12-07 22:30:00 +0300

    Lib

---
+++ pizzaStore.php
@@ -0,0 +1,21 @@
+<?php
+namespace PizzaLib;
+class PizzaStore
+{
+    public function orderPizza(string $type)
+    {
+        $pizza = $this->createPizza($type);
+        $pizza->prepare();
+        $pizza->cut();
+        return $pizza;
+    }
+    protected function createPizza(string $type)
+    {
+        switch (strtolower($type)) {
+            case 'margarita':
+                return new Pizza('Маргарита', 'томатный соус', ['сыр']);
+            case 'pepperoni':
+                return new Pizza('Пепперони', 'томатный соус', ['сыр', 'колбаса']);
+        }
+    }
+}
